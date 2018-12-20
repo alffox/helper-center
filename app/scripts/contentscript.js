790faf89aee162f22bdfc8a9172d4bf096cc328d
alffox <alfonso.crisci.it@gmail.com>
2018-12-20 13:51:16 +0100

    feat: Get current URL

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,6 +2,8 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message === 'tab switched' && request.url.indexOf('https://liferay-support.zendesk.com/agent/tickets/') == 0) {
             console.log(request.url);
+
+            const currentURL = window.location.href;
         }
     });
 
