3fce576ef2b80051b0a9fdf5b91e39eb04372bcf
alffox <alfonso.crisci.it@gmail.com>
2018-12-20 14:38:01 +0100

    feat: Construct Project info URL

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -19,6 +19,7 @@ chrome.runtime.onMessage.addListener(
                     const OSBKey = data.ticket.custom_fields[1].value;
                     const patcherURL = "https://patcher.liferay.com/group/guest/patching/-/osb_patcher/accounts/view?_1_WAR_osbpatcherportlet_patcherBuildAccountEntryCode=" + accountCode;
                     const LESAlikeFormat = accountCode + "-" + ticketID;
+                    const projectInfoURL = "https://customer.liferay.com/project-details?p_p_id=com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_mvcRenderCommandName=/view_account_entry&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_redirect=https://customer.liferay.com/project-details?p_p_id=com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_cur=1&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_delta=20&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_keywords=carrefour&_com_liferay_osb_customer_account_entry_details_web_AccountEntryDetailsPortlet_accountEntryId=" + OSBKey;
                 }
             });
         }
